5df0b0f4f6d67e5691b7097fcaeac6b54c1a0153
Ross Wardrup <minorsecond@gmail.com>
2021-08-25 14:24:12 -0600

    Use std::array for state collection

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -28,7 +28,7 @@ MainWindow::MainWindow(QWidget *parent)
     ut.create_directories();
 
     // Populate the state selection combo boxes
-    const std::vector<QString> states {"Alabama", "Alaska", "Arizona", "Arkansas", "California",
+    const std::array<QString, 50> states {"Alabama", "Alaska", "Arizona", "Arkansas", "California",
                                           "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
                                           "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
                                           "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
